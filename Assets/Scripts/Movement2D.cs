461129b8b30efb8c695d70445f2adf100a35be6d
huynh_do <huynh.fetel@gmail.com>
2019-09-29 13:44:47 +0700

    set-up input and add simple player movement controller scripts

---
+++ Assets/Scripts/Movement2D.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum PlayerInput
+{
+    P1 = 1,
+    P2 = 2
+}
+
+public class Movement2D : MonoBehaviour
+{
+    private float moveSpeed;
+
+    private string horizontalAxis;
+    private string verticalAxis;
+
+    private string fire1Btn;
+    private string fire2Btn;
+    private string fire3Btn;
+
+    bool isDoneSetup;
+
+
+    public System.Action OnClickFire1Action;
+    public System.Action OnClickFire2Action;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDoneSetup)
+        {
+            if (Input.GetAxisRaw(horizontalAxis) != 0 || Input.GetAxisRaw(verticalAxis) != 0)
+                Move();
+
+            if (Input.GetButtonUp(fire1Btn))
+                if (OnClickFire1Action != null)
+                    OnClickFire1Action.Invoke();
+
+            if (Input.GetButtonUp(fire2Btn))
+                if (OnClickFire2Action != null)
+                    OnClickFire2Action.Invoke();
+        }
+    }
+
+    private void Move()
+    {
+        Vector3 rightMovement = Vector3.right * moveSpeed * Time.deltaTime * Input.GetAxisRaw(horizontalAxis);
+        Vector3 upMovement = Vector3.up * moveSpeed * Time.deltaTime * Input.GetAxisRaw(verticalAxis);
+        Vector3 heading = Vector3.Normalize(rightMovement + upMovement);
+
+        transform.position += rightMovement;
+        transform.position += upMovement;
+
+       //UpdateAnimation(heading);
+    }
+    
+    public void SetPlayerInput(PlayerInput playerInput)
+    {
+        switch (playerInput)
+        {
+            case PlayerInput.P1:
+                {
+                    horizontalAxis = "P1_Horizontal";
+                    verticalAxis = "P1_Vertical";
+
+                    fire1Btn = "P1_Fire1";
+                    fire2Btn = "P1_Fire2";
+                    fire3Btn = "P1_Fire3";
+                }
+                break;
+            case PlayerInput.P2:
+                {
+                    horizontalAxis = "P2_Horizontal";
+                    verticalAxis = "P2_Vertical";
+
+                    fire1Btn = "P2_Fire1";
+                    fire2Btn = "P2_Fire2";
+                    fire3Btn = "P2_Fire3";
+                }
+                break;
+            default:
+                {
+                    horizontalAxis = "Horizontal";
+                    verticalAxis = "Vertical";
+
+                    fire1Btn = "Fire1";
+                    fire2Btn = "Fire2";
+                    fire3Btn = "Fire3";
+                }
+                break;
+        }
+        isDoneSetup = true;
+    }
+    
+    public void SetMovementSpeed( float moveSpeed)
+    {
+        this.moveSpeed = moveSpeed;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] PlayerInput playerInput;
+    [SerializeField] private Movement2D movementInput;
+
+    
+    /// <summary>
+    /// number of bomb in player 
+    /// </summary>
+    private int bombQuantity;
+    private int playerHealth = 1;
+    /// <summary>
+    /// speed of player
+    /// </summary>
+    [SerializeField]  private int playerRunSpeed;
+    /// <summary>
+    /// 
+    /// </summary>
+    private int blastRange;
+
+    [SerializeField] private GameObject bombPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Init()
+    {
+        movementInput.SetMovementSpeed(playerRunSpeed);
+        movementInput.SetPlayerInput(playerInput);
+
+        movementInput.OnClickFire1Action = PutBomb;
+        movementInput.OnClickFire2Action = PutBomb;
+    }
+
+    void PutBomb()
+    {
+        Debug.Log("Putbomb ");
+
+        var bomb = Instantiate(bombPrefab);
+        bomb.transform.position = transform.position;
+    }
+
+    void RemoteBomb()
+    {
+        Debug.Log("RemoteBomb ");
+    }
+}
